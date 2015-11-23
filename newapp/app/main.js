7e1e225eb78edc5fd1239ee8abba39d9f5660ceb
Charles F. Munat <charles.munat@gmail.com>
2015-11-23 15:29:26 +1300

    Make board from moves array

--- newapp/app/main.js
+++ newapp/app/main.js
@@ -1,18 +1,19 @@
-import { indexOf } from 'ramda'
+import { addIndex, indexOf, map } from 'ramda'
 
-const getPlayerFromMove = function (square) {
-  const moves = [4, 3, 0, 8, 2, 1, 6]
-  const idx = indexOf(square, moves)
+const makeBoard = function (moves) {
+  const mapIndexed = addIndex(map)
 
-  if (idx === -1) {
-    return ''
-  } else {
-    return (idx % 2 === 0) ? 'X' : 'O'
+  const getPlayerFromMove = function (square) {
+    const idx = indexOf(square, moves)
+
+    if (idx === -1) {
+      return ''
+    } else {
+      return (idx % 2 === 0) ? 'X' : 'O'
+    }
   }
+
+  return mapIndexed((_, idx) => getPlayerFromMove(idx), new Array(9))
 }
 
-console.log("What's in cell 4 (center)? " + getPlayerFromMove(4))
-console.log("What's in cell 3 (middle left)? " + getPlayerFromMove(3))
-console.log("What's in cell 1 (center top)? " + getPlayerFromMove(1))
-console.log("What's in cell 2 (top right)? " + getPlayerFromMove(2))
-console.log("What's in cell 2 (middle right)? " + getPlayerFromMove(5))
+console.log('The board: ' + makeBoard([4, 3, 0, 8, 2, 1, 6]))
