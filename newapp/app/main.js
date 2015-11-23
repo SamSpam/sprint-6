28bbe9e1dd1c8df003ff76934195f0aec2e0da32
Charles F. Munat <charles.munat@gmail.com>
2015-11-23 14:44:19 +1300

    Testing again

--- newapp/app/main.js
+++ newapp/app/main.js
@@ -1,26 +1,27 @@
 import { filter } from 'ramda'
 
-const wonGame = function (board, player, win) {
-  console.log("The board: ", board)
-  console.log("The player: ", player)
-  console.log("The win: ", win.join("|"))
+const wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
-  let len = filter(
-    function (cell) {
-      console.log("Checking index: ", cell)
-      console.log("In cell: ", board[cell])
-
-      return board[cell] === player
-    }, win
-  ).length
-
-  console.log("Number of ", player, " = ", len)
-
-  return len === 3
+const isAWin = function (board, player, win) {
+  return filter(function (i) { return board[i] === player }, win).length === 3
 }
 
-let win = [0, 4, 8]
-let player = 'X'
-let board = ['X', 'O', 'X', 'O', 'X', '', 'X', '', 'O']
+const winner = ['X', 'O', 'X', 'O', 'X', '', 'X', '', 'O',]
 
-console.log(wonGame(board, player, win))
+console.log("Top row: " + isAWin(winner, 'X', wins[0]))
+console.log("Middle row: " + isAWin(winner, 'X', wins[1]))
+console.log("Bottom row: " + isAWin(winner, 'X', wins[2]))
+console.log("Left column: " + isAWin(winner, 'X', wins[3]))
+console.log("Middle column: " + isAWin(winner, 'X', wins[4]))
+console.log("Right column: " + isAWin(winner, 'X', wins[5]))
+console.log("Top left to bottom right: " + isAWin(winner, 'X', wins[6]))
+console.log("Top right to bottom left: " + isAWin(winner, 'X', wins[7]))
